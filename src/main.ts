a9c7db692fa081a51ae1754f069f84c32c8844a3
MouadBNL <mouadbnl.2k01@gmail.com>
2021-11-07 13:59:29 +0100

    installed pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { router } from './router'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
