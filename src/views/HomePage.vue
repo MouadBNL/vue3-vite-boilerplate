99dd57bfea9c4e97119bbaec5fbddc4bf7746b3b
MouadBNL <mouadbnl.2k01@gmail.com>
2021-11-07 13:53:43 +0100

    showing error on axios

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
         <h1>Home page</h1>
-        <button @click="handleClick">Send request</button>
-        <pre>{{data}}</pre>
+        <button @click="handleClick">Send request</button> <span>{{ isLoading ? 'Loading...': 'Finished'}}</span>
+        <div style="display: flex; gap: 1rem;">
+            <div>
+                <p>Data</p>
+                <pre>{{data}}</pre>
+            </div>
+            <div>
+                <p>Error</p>
+                <pre>{{error}}</pre>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
 import { useAxios } from '@/services/use-axios'
-const { fetch, data } = useAxios('https://jsonplaceholder.typicode.com/users')
+const { fetch, data, error, isLoading } = useAxios('https://jsonplaceholder.typicode.com/users')
 const handleClick = async () => {
     await fetch()
     console.log(data.value)
