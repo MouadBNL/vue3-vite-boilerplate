c0591bee84268fad79c067560d54cf9f90055fba
MouadBNL <mouadbnl.2k01@gmail.com>
2021-11-07 13:45:46 +0100

    added axios as a service

---
+++ src/services/use-axios.ts
@@ -0,0 +1,42 @@
+import { ref } from 'vue'
+import axios, { AxiosResponse } from "axios"
+
+const apiClinet = axios.create({
+    baseURL: 'http://localhost/api/v1'
+})
+
+export const useAxios = <T = any>(url: string, config: Object= {}) => {
+
+    const response = ref<AxiosResponse<T>>()
+    const data = ref<T>()
+    const error = ref<any>()
+    const isLoading = ref<boolean>(false)
+
+    const fetch = async () => {
+        isLoading.value = true
+        try {
+            const res = await apiClinet.request({
+                url,
+                ...config
+            })
+            response.value = res
+            data.value = res.data
+        } catch (err: any) {
+            error.value = err
+            throw err
+        } finally {
+            isLoading.value = false
+        }
+    }
+
+    return { response, data, error, isLoading, fetch }
+}
+
+export const setAuthorizationHeader = (token: string | null) => {
+    apiClinet.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    if(token) {
+        localStorage.setItem('jwt_token', token)
+    } else {
+        localStorage.removeItem('jwt_token')
+    }
+}
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         <h1>Home page</h1>
+        <button @click="handleClick">Send request</button>
+        <pre>{{data}}</pre>
     </div>
 </template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { useAxios } from '@/services/use-axios'
+const { fetch, data } = useAxios('https://jsonplaceholder.typicode.com/users')
+const handleClick = async () => {
+    await fetch()
+    console.log(data.value)
+}
+</script>
